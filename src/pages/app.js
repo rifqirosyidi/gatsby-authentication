a052d921f58bf4378fcee3e954db66633ea0fed8
Rifqi Rosyidi <rief.rosyidi@gmail.com>
2021-01-30 12:44:59 +0700

    implement private route

--- src/pages/app.js
+++ src/pages/app.js
@@ -1,12 +1,13 @@
 import React from "react"
 import { Router } from "@reach/router"
 import Layout from "../components/layout"
+import PrivateRoute from "../components/privateRoute"
 import Profile from "../components/profile"
 import Login from "../components/login"
 const App = () => (
   <Layout>
     <Router>
-      <Profile path="/app/profile" />
+      <PrivateRoute path="/app/profile" component={Profile} />
       <Login path="/app/login" />
     </Router>
   </Layout>
